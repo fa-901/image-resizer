2002aa2ac460e75c66e637e5107fdac41a0c0750
Farhan Alam <metalhead901@live.com>
2021-10-16 10:36:15 +0600

    added proxy port for frontend + others
    - Added nodemon for server development
    - add scripts for running server & client from root
    - test backend connectivity with frontend in App

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
 	res.json({ message: "Yo, this server is running!" });
@@ -10,4 +13,8 @@ app.get("/api", (req, res) => {
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
