d1614b2b5fbf84bc57172313e71329b3f6a7ca03
Farhan Alam <metalhead901@live.com>
2021-10-16 11:42:32 +0600

    Added Tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ function App() {
 			.then((res) => { return res.json() })
 			.then((data) => console.log(data.message));
 	}, []);
+
 	return (
-		<div className="App">
-			<header>
+		<div className="font-mono container mx-auto h-screen text-center py-10 flex flex-col items-center">
+			<header className='text-2xl mb-5'>
 				Image Resizer
 			</header>
 			<main>
